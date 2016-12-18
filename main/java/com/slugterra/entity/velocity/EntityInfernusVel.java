6e75415ceae100500bd2b299066dacad469f5f46
mpbagot <packof14eggies@gmail.com>
2016-12-18 20:57:01 +1100

    Added Infernus Abilities

--- main/java/com/slugterra/entity/velocity/EntityInfernusVel.java
+++ main/java/com/slugterra/entity/velocity/EntityInfernusVel.java
@@ -48,7 +48,8 @@ public class EntityInfernusVel extends EntityVel{
 			//firewall ability
 			if (abilint == 0){
 				System.out.println("Activating firewall!!");
-				this.worldObj.newExplosion(this, this.posX, this.posY, this.posZ, 3.0F, true, false);
+				this.generateFireWall();
+				//this.worldObj.newExplosion(this, this.posX, this.posY, this.posZ, 3.0F, true, false);
 				System.out.println("Activating explosion on server side");
 				this.setDead();
 			}
@@ -100,32 +101,40 @@ public class EntityInfernusVel extends EntityVel{
 			}
 
 			else if (abilint == 3){
+				this.makeTrench(true);
 				System.out.println("Activating NovaClaw!!");
-				
+
 			}
 
 		}else{
 			//wingburst ability (OP!!)
 			if (abilint == 0){
 				System.out.println("Activating wingburst!!");
-				for (int i=0;i<24;i++){
-					double x = this.posX + new Random().nextInt(i);
-					double y = this.posY + new Random().nextInt(i);
-					double z = this.posZ + new Random().nextInt(i);
-					EntityFireball fireball = new EntitySmallFireball(this.worldObj, x, y, z, this.motionX, this.motionY, this.motionZ);
+				for (int i=1;i<24;i++){
+					boolean plus = new Random().nextBoolean();
+					int plus2;
+					if (plus)
+						plus2 = -1;
+					else
+						plus2 = 1;
+					double x = this.posX + new Random().nextInt(i)*plus2;
+					double y = this.posY + new Random().nextInt(i)*plus2;
+					double z = this.posZ + new Random().nextInt(i)*plus2;
+					EntityFireball fireball = new EntitySmallFireball(this.worldObj, x, y, z, this.motionX, this.motionY-0.5f, this.motionZ);
 					this.worldObj.spawnEntityInWorld(fireball);
 				}
-				this.worldObj.newExplosion(this, this.posX, this.posY, this.posZ, 5.0f, false, true);
+				this.worldObj.newExplosion(this, this.posX, this.posY, this.posZ, 1.0f, false, true);
 				this.setDead();
 			}
-			
+
 			//beatwave ability
 			else if (abilint == 1){
 				System.out.println("Activating beatwave!!");
+				this.setDead();
 				EntityFireball fireball = new EntityLargeFireball(this.worldObj, this.posX, this.posY, this.posZ, this.motionX, this.motionY, this.motionZ);
 				this.worldObj.spawnEntityInWorld(fireball);
 			}
-			
+
 			//flashfire ability
 			else if (abilint == 2){
 				System.out.println("Activating flashfire!!");				
@@ -133,5 +142,48 @@ public class EntityInfernusVel extends EntityVel{
 				this.worldObj.spawnEntityInWorld(fireball);
 			}
 		}
+	}
+
+	private void makeTrench(boolean deep) {
+		if (this.motionX > 0){
+			if (this.motionX > this.motionZ){
+				this.makeTrenchSingle(1, 0, deep);
+			} else {
+				this.makeTrenchSingle(0, 1, deep);
+			}
+		} else {
+			if (this.motionX < this.motionZ){
+				this.makeTrenchSingle(-1, 0, deep);
+			} else {
+				this.makeTrenchSingle(0, -1, deep);
+			}
+		}
+
+	}
+
+	private void makeTrenchSingle(int b, int c, boolean deep) {
+		for (int a=0;a < 11; a++){
+			this.makePlus((int)this.posX+a*b, (int)this.posY, (int)this.posZ+a*c, false);
+			if (deep)
+				this.makePlus((int)this.posX+a*b, (int)this.posY-1, (int)this.posZ+a*c, true);
+		}
+	}
+	
+	private void makePlus(int x, int y, int z, boolean trench){
+		for (int a=-1;a<2;a++){
+			for (int b=-1;b<2;b++){
+				this.worldObj.setBlock(x+a, y, z+b, Blocks.fire);
+				if (trench && a==0 && b==0){
+					this.worldObj.setBlock(x, y-1, z, Blocks.fire);
+				}
+			}
+		}
+	}
+
+	private void generateFireWall() {
+		double x = this.motionX;
+		this.motionX = this.motionZ;
+		this.motionZ = x;
+		this.makeTrench(false);
 	}
 }
